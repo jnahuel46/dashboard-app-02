a10c95d1438c4bc618b1b3f39e8a5b84c43d2f68
Jeremias <jnahuel46@gmail.com>
2024-03-10 02:15:20 -0300

    pokemons page finish with not found page and extra layout

--- src/app/dashboard/pokemons/page.tsx
+++ src/app/dashboard/pokemons/page.tsx
@@ -1,21 +1,21 @@
 import { PokemonGrid, PokemonsResponse, SimplePokemon } from "@/app/pokemons";
-import Image from "next/image";
 
 const getPokemons = async (
   limit = 30,
   offset = 0
 ): Promise<SimplePokemon[]> => {
   const data: PokemonsResponse = await fetch(
-    `https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`
+    `https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`,
+    { cache: "force-cache" }
   ).then((res) => res.json());
 
   const pokemons = data.results.map((pokemon: any) => ({
     id: pokemon.url.split("/").at(-2)!,
     name: pokemon.name,
   }));
-  
-  throw new Error("testing");
- 
+
+  //throw new Error("testing");
+
   return pokemons;
 };
 
